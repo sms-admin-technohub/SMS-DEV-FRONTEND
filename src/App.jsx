51796e7015aa8b42c125d30f02a8f24a9afc98d1
Sameera <sameeramansoori08@gmail.com>
2024-05-17 08:44:59 +0200

    Added students pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
 import Dashboard from './pages/dashboard/Dashboard'
 import { Routes, Route } from 'react-router-dom'
-// import Profiles from './pages/profiles/Profiles'
-import AddStudentForm from './pages/students/AddStudents'
+import Departments from './pages/departments/Departments'
 import Teachers from './pages/teachers/Teachers'
-// import Tasks from './pages/tasks/Tasks'
+import Subjects from './pages/subjects/Subjects'
 import DefaultLayout from './_components/DefaultLayout'
-import EditStudentForm from './pages/students/EditStudents'
-// import LIst from './pages/students/LIst'
-import Table from './components/Table'
-import StudentView from './pages/students/StudentView'
-import StudentList from './pages/students/StudentList'
+import Accounts from './pages/accounts/Accounts'
+import Events from './components/Events';
+import Examlist from './components/Examlist';
+import Fees from './components/Fees'
+import Library from './components/Library'
+import Holiday from './components/Holiday'
+import Timetable from './components/Timetable'
+import Settings from './components/Settings'
+import AddStudentForm from './pages/students/AddStudents'
+import EditStudentForm from './pages/students/EditStudents';
+import StudentList from './pages/students/StudentList';
+import StudentView from './pages/students/StudentView';
+
 function App() {
 
   return (
     // here you replace these components with your actual components!
     <Routes>
+      {/* main menu routes */}
       <Route path='/' element={<DefaultLayout />} >
         <Route path='/dashboard' element={<Dashboard />} />
-        {/* <Route path='/profiles' element={<Profiles />} /> */}
+        <Route path='/departments' element={<Departments />} />
+        <Route path='/teachers' element={<Teachers />} />
+
+        <Route path='/subjects' element={< Subjects />} />
+        <Route path='/accounts' element={< Accounts />} />
         <Route path='/addstudents' element={<AddStudentForm />}/>
         <Route path='/editstudents' element={<EditStudentForm />}/>
         <Route path='/studentlist' element={<StudentList />} />
-        <Route path='/Table' element={<Table />} />
         <Route path='/viewstudents' element={<StudentView />} />
 
-
-        <Route path='/teachers' element={<Teachers />} />
-        {/* <Route path='/tasks' element={< Tasks />} /> */}
-
+        {/* managment menu routes */}
+        <Route path='/events' element={< Events />} />
+        <Route path='/examlist' element={< Examlist />} />
+        <Route path='/fees' element={< Fees />} />
+        <Route path='/library' element={< Library />} />
+        <Route path='/holiday' element={< Holiday />} />
+        <Route path='/accounts' element={< Accounts />} />
+        <Route path='/timetable' element={< Timetable />} />
+        <Route path='/settings' element={< Settings />} />
       </Route>
     </Routes>
-
   )
 }
-
 export default App
+
--- src/pages/students/StudentView.jsx
+++ src/pages/students/StudentView.jsx
@@ -1,109 +1,90 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-function StudentView() {
-  return (
-    <div className='h-screen bg-gray-100 overflow-hidden'>
-      <div className='container mx-auto px-4'>
-        <div className='flex justify-between items-center py-4'>
-          <h1 className='text-2xl font-semibold'>Student Details</h1>
-          <div className="flex">
-            <h1 className='pr-2 font-semibold text-md'>
-              <Link to='/studentlist'>Student</Link>
-            </h1>    
-            <h1 className='flex justify-end font-semibold text-md'>/Students Details</h1>
-          </div>
-        </div>
-        {/* ================ */}
+import { faBuilding, faCalendarDays, faEnvelope, faItalic, faLocationDot, faPhoneVolume, faUser } from '@fortawesome/free-solid-svg-icons';
 
 
-        <div className='main '>
+const studentsView = () => {
+    return (
+        <>
+            <div className="flex flex-col lg:flex-row justify-between">
+                <div className=" mr-7 pr-10 text-left lg:ml-10 my-10 rounded-lg border border-black-100 py-6 px-6
+                w-full 
+              xl:w-1/2 xl:px-6
+              lg:w-2/5 lg:px-6 
+              md:px-10
+             ">
+                    <h1 className="text-xl mb-10 font-semibold ">Personal Details :</h1>
+                    <div>
+                        <div className="mb-4">
 
-        <div className="grid grid-cols-5 grid-rows-5 gap-4">
-         <div className="col-span-2 bg-white ml-10 h-[550px] w-[400px] row-span-5">
-         <h2 className="text-xl font-semibold mb-4">Personal Details</h2>
-              
-              <p className='font font-bold text-lg'>Name</p>
-              <p className='text-gray-600 font-medium pb-3'> Sameera</p>
-
-              <p className='font font-bold text-lg'>Department</p>
-              <p className='text-gray-600 font-medium pb-3'>EC</p>
-
-              <p className='font font-bold text-lg'>Mobile</p>
-              <p className='text-gray-600 font-medium pb-3'>6768937973</p>
-
-              <p className='font font-bold text-lg'>Email</p>
-              <p className='text-gray-600 font-medium pb-3'>sameeramansoori08@gmail.com</p>
-
-              <p className='font font-bold text-lg'>Gender</p>
-              <p className='text-gray-600 font-medium pb-3'>female</p>
-
-              <p className='font font-bold text-lg'>Date of Birth</p>
-              <p className='text-gray-600 font-medium pb-3'>2 aug 2004</p>
-
-              <p className='font font-bold text-lg'>Language</p>
-              <p className='text-gray-600 font-medium pb-3'>English</p>
-
-              <p className='font font-bold text-lg'>Address</p>
-              <p className='text-gray-600 font-medium pb-3'>Bhopal</p>
-    </div>
-    <div className="col-span-3 bg-white mr-20 h-[550px] w-[800px] row-span-5 col-start-3">2</div>
-</div>
-    
-
-
-
-        </div>
-
-        {/* ============== */}
-        {/* <div className='bg-white rounded-lg shadow-md px-6 py-8 md:py-12'>
-          <div className='flex justify-between items-center mb-8'>
-            <h1 className='text-2xl font-semibold'>Profile</h1>
-            <h1 className='text-xl'>...</h1>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="bg-white w-[350px] rounded-lg border border-gray-300 text text-start pl-10 py-4">
-              <h2 className="text-xl font-semibold mb-4">Personal Details</h2>
-              <p className='font font-bold text-lg'>Name</p>
-              <p className='text-gray-600 font-medium pb-3'> Sameera</p>
-
-              <p className='font font-bold text-lg'>Department</p>
-              <p className='text-gray-600 font-medium pb-3'>EC</p>
-
-              <p className='font font-bold text-lg'>Mobile</p>
-              <p className='text-gray-600 font-medium pb-3'>6768937973</p>
-
-              <p className='font font-bold text-lg'>Email</p>
-              <p className='text-gray-600 font-medium pb-3'>sameeramansoori08@gmail.com</p>
-
-              <p className='font font-bold text-lg'>Gender</p>
-              <p className='text-gray-600 font-medium pb-3'>female</p>
-
-              <p className='font font-bold text-lg'>Date of Birth</p>
-              <p className='text-gray-600 font-medium pb-3'>2 aug 2004</p>
-
-              <p className='font font-bold text-lg'>Language</p>
-              <p className='text-gray-600 font-medium pb-3'>English</p>
-
-              <p className='font font-bold text-lg'>Address</p>
-              <p className='text-gray-600 font-medium pb-3'>Bhopal</p>
-              
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faUser} />
+                                Name</h3>
+                            <p className='pl-7'>Bruce Willis</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faBuilding} />Department</h3>
+                            <p className='pl-7'>Computer Science</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faPhoneVolume}/>Mobile</h3>
+                            <p className='pl-7'>+91 89657 48512</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faEnvelope} />Email</h3>
+                            <p className='pl-7'>joe@gmail.com</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faUser} />Gender</h3>
+                            <p className='pl-7'>Male</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faCalendarDays}/>Date of Birth</h3>
+                            <p className='pl-7'>11 May 2024</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4'icon={faItalic} />Language</h3>
+                            <p className='pl-7'>English, French, Bangla</p>
+                        </div>
+                        <div>
+                            <h3 className="font-semibold"><FontAwesomeIcon className='pr-4' icon={faLocationDot} />Address</h3>
+                            <p className='pl-7'>480, Eastern Avenue, New York</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full  
+              xl:w-2/3 xl:px-6
+              lg:w-2/3 lg:px-6 
+               md:pl-10 md:px-10
+               text-left my-10 mr-10 rounded-md border border-black-100 py-6 px-6">
+                    <h1 className="text-xl mb-10 font-semibold ">About Me</h1>
+                    <div>
+                        <h2 className="text-lg mb-4 font-semibold">Hello, I am Daisy Parks</h2>
+                        <p className="mb-4"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni possimus culpa dolore minus quo error sequi ex cupiditate! Fuga architecto fugiat aliquam ipsa quas cumque sed quam quisquam dolor. Pariatur, eos quas quod corporis perspiciatis amet! Alias eligendi, aperiam voluptatibus harum expedita itaque animi. Sed vel quia voluptatibus minus?</p>
+                        <p className="mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, eos optio aut hic ratione beatae. Quam quo dignissimos alias deleniti!</p>
+                        <div>
+                            <h3 className="text-lg font-semibold mb-4 ">Education</h3>  
+                            <div className="mb-2">
+                                <h6 className="font-semibold">2008 - 2009</h6>
+                                <p>btech in ec,canada</p>
+                            </div>
+                            <div className="mb-2">
+                                <h6 className="font-semibold">2011 - 2012</h6>
+                                <p>Higher Secondary Schooling ,usa</p>
+                            </div>
+                            <div className="mb-2">
+                                <h6 className="font-semibold">2012 - 2015</h6>
+                                <p>Bachelor of computer administration,australia </p>
+                            </div>
+                            <div className="mb-2">
+                                <h6 className="font-semibold">2015 - 2017</h6>
+                                <p>Master of Science ,dubai</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="bg-white rounded-lg border border-gray-300 px-6 py-4">
-              <h2 className="text-xl font-semibold mb-4">About Me</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                tincidunt justo id ex pulvinar, et placerat arcu vestibulum. Duis
-                sed leo vitae ligula tincidunt malesuada. Phasellus ut nibh nec urna dictum
-                ultrices. Integer nec dictum risus.
-              </p>
-            </div>
-          </div>
-        </div> */}
-      </div>
-    </div>
-  );
+        </>
+    );
 }
 
-export default StudentView;
+export default studentsView;

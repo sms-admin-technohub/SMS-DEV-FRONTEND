5740f3818be5d864d35a9abf7144a770d76e8758
abdularham88 <m888arham@gmail.com>
2024-05-21 17:03:11 +0530

    Added class,classroom,classroom table and teachers

---
+++ src/pages/classroom_type/AddClassroomType.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+
+const AddClassroomType = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [file, setFile] = useState(null);
+
+    const onSubmit = (data) => {
+        console.log(data);
+        console.log(file);
+    };
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+
+    return (
+        <div className='container bg bg-gray-100 h-screen w-full overflow-hidden'>
+            <div className='flex justify-between'>
+                <h1 className='pl-8  pt-4 font-semibold text-2xl'> Add Classrooms</h1>
+                <div className="flex">
+                    <h1 className='pr-2 pt-4 font-semibold text-md'>
+                        Classroom
+                    </h1>
+                    <h1 className='flex justify-end mt-4  pr-6 font-semibold text-md'>/Add Classrooms</h1>
+                </div>
+            </div>
+
+            <div className='form bg-white mt-8  mx-6 rounded-lg'>
+                <div className='text text-start pl-3 pt-4 font-semibold text-xl'><h1>Classroom Information</h1></div>
+
+                <form onSubmit={handleSubmit(onSubmit)} className="max-w-full mt-5 mx-4 ">
+
+                    <div className="mb-4  ">
+                        <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
+                            <div>
+                                <label className="block text-sm font-bold mb-2 focus:out  ">First Name</label>
+                                <input {...register("firstName", { required: true })} type="text" placeholder="Enter First Name"
+                                    className="  focus:border-blue-500 shadow appearance-none border rounded w-full   py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                {errors.firstName && <span className="text-red-500 text-xs">This field is required</span>}
+                            </div>
+
+                            <div >
+                                <label className="block text-sm font-bold mb-2">Middle Name</label>
+                                <input {...register("middleName", { required: true })} type="text" placeholder="Enter Middle Name" className=" focus:border-blue-500 shadow appearance-none border rounded w-full   py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                {errors.middleName && <span className="text-red-500 text-xs">This field is required</span>}
+
+                            </div>
+
+                            <div >
+                                <label className="block text-sm font-bold mb-2">Last Name</label>
+                                <input {...register("lastName", { required: true })} type="text" placeholder="Enter Last Name" className=" focus:border-blue-500 shadow appearance-none border rounded w-full   py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                {errors.lastName && <span className="text-red-500 text-xs">This field is required</span>}
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="mb-4  ">
+                        <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
+                            <div >
+                                <label className="block text-sm font-bold mb-2">E-mail</label>
+                                <input {...register("email", { required: true })} type="text" placeholder="Enter Email Address" className=" focus:border-blue-500 shadow appearance-none border rounded w-full   py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                {errors.email && <span className="text-red-500 text-xs">This field is required</span>}
+                            </div>
+                            <div >
+                                <label className="block text-sm font-bold mb-2">Password</label>
+                                <input {...register("password", { required: true })} type="text" placeholder="Enter Password " className=" focus:border-blue-500 shadow appearance-none border rounded w-full   py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                {errors.password && <span className="text-red-500 text-xs">This field is required</span>}
+                            </div>
+                            <div >
+                                <label className="block text-sm font-bold mb-2">Phone Number</label>
+                                <input {...register("phone", { required: true })} type="text" placeholder="Enter Phone Number " className=" focus:border-blue-500 shadow appearance-none border rounded w-full   py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+                                {errors.phone && <span className="text-red-500 text-xs">This field is required</span>}
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="flex justify-start font-bold pt-4">
+                        <h1>Upload Student Photo (150px X 150px)</h1>
+                    </div>
+
+                    <div className="flex justify-start mb-4 pb-2">
+                        <div className="mr-4">
+                            <label className="block text-sm font-bold mb-2 "></label>
+                            <input onChange={handleFileChange} type="file" />
+                        </div>
+                    </div>
+
+                    <div className="flex justify-start mb-4 pb-6">
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  rounded focus:outline-none focus:shadow-outline">Submit</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    );
+}
+
+export default AddClassroomType;
